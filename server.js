2f76370b29094913a1986187e2f84a3f5072a62e
LeandroLedesma <leandro.d.ledesma@gmail.com>
2018-10-18 22:10:24 -0300

    create server, create view home

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, '/views/index.html'));
+});
+ 
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
